3e2a032ecc596baeb7d6e4bb7755c4693d0566fc
oksihawini-oss <oksihawini@gmail.com>
2026-06-20 17:49:46 +0800

    Tugas akhir : Pengelola Data Barang

---
+++ src/bagian4/tugas/Gudang.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bagian4.tugas;
+
+/**
+ *
+ * @author ASUS Vivobook
+ */
+public class Gudang {
+    
+}
